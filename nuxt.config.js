72b5cbb942556ba35a60948bb16bbd33927c35a2
Masahiro Harada <masahiro.harada@ari-jp.com>
2018-12-29 18:59:01 +0900

    First commit

---
+++ nuxt.config.js
@@ -0,0 +1,66 @@
+const pkg = require('./package')
+require('dotenv').config()
+
+module.exports = {
+  mode: 'universal',
+
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: pkg.name,
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: pkg.description }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+
+  /*
+  ** Global CSS
+  */
+  css: [
+  ],
+
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+  ],
+
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    '@nuxtjs/axios',
+    '@nuxtjs/dotenv'
+  ],
+
+  /*
+  ** Axios module configuration
+  */
+  axios: {
+    baseURL: process.env.API_URL,
+    credentials: true
+  },
+
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend(config, ctx) {
+
+    }
+  }
+}
---
+++ store/auth.js
@@ -0,0 +1,32 @@
+export const state = () => ({
+  user: null
+})
+
+export const mutations = {
+  setUser (state, user) {
+    state.user = user
+  }
+}
+
+export const actions = {
+  async login ({ commit }, { email, password }) {
+    const response = await this.$axios.$post('/login', { email, password })
+      .catch(err => err.response)
+
+    if (response.status !== 200) {
+      // TODO
+    }
+
+    commit('setUser', response)
+  },
+  async logout ({ commit }) {
+    const response = await this.$axios.$post('/logout')
+      .catch(err => err.response)
+
+    if (response.status !== 200) {
+      // TODO
+    }
+
+    commit('setUser', null)
+  }
+}
